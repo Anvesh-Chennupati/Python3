91dc9091c8c68dd6841390f7d85ba97c3d44d5a3
Anvesh Chennupati <anveshchowdary01@gmail.com>
2018-02-15 10:04:23 +0530

    Packages Versions

---
+++ DataCamp/Versions.py
@@ -0,0 +1,11 @@
+import numpy
+import pandas
+import sklearn
+import matplotlib
+import scipy
+
+print('\n Numpy version :' + numpy.__version__)
+print('\n Scipy version :' + scipy.__version__)
+print('\n Matplotlib version :' + matplotlib.__version__)
+print('\n Pandas version :' + pandas.__version__)
+print('\n SciKit-Learn version :' + sklearn.__version__)
