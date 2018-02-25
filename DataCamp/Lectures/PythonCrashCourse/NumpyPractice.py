635ab781476059969d37fec72b49e002be69089a
anvesh-chennupati <anveshchowdary01@gmail.com>
2018-02-25 21:33:27 +0530

    Numpy

--- DataCamp/Lectures/PythonCrashCourse/NumpyPractice.py
+++ DataCamp/Lectures/PythonCrashCourse/NumpyPractice.py
@@ -2,10 +2,18 @@ import numpy as np
 
 d1 = np.arange(1,100,5)
 
-print(d1)
+#print(d1)
 
 d2 = np.zeros((5,5))
 
-print(d2)
+#print(d2)
 
-print('\n',np.eye(5))
+#print('\n',np.eye(5))
+
+n1 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#print(n1[:2,:2])
+
+mat1 = np.zeros((10,10))
+
+print(mat1.shape[1])
