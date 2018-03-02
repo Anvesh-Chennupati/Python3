afd6b5477a926ae66bd0b27ab228425f60b8b1c5
anvesh-chennupati <anveshchowdary01@gmail.com>
2018-03-02 21:22:48 +0530

    Data Frames operations

--- DataCamp/Lectures/PythonCrashCourse/PandasPractice.py
+++ DataCamp/Lectures/PythonCrashCourse/PandasPractice.py
@@ -21,3 +21,29 @@ print(pseries1 + pseries3)
 np.random.seed(1) #To keep the randn values same for all the runs
 df = pd.DataFrame(np.random.randn(5,4),['A','B','C','D','E'],['W','X','Y','Z'])
 print(df)
+print(df['W'])
+print(df[['W','Z']])
+
+#creating a new column in df
+
+df['new'] = df['W'] + df['Y']
+
+print(df)
+
+print(df.drop('new',axis=1)) #it does not actually delete the data
+
+print(df)
+
+#in order to delte the data we have to mention inplace = True and specify the axis as 1 for column
+
+df.drop('new',axis=1,inplace=True)
+
+print(df)
+
+#To drop a row
+
+print(df.drop('E',axis=0))
+
+df.drop('E',axis = 0,inplace=True)
+
+print(df)
