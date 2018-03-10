74d7633e92927890ea2c2e66ef19a51684a654bb
anvesh-chennupati <anveshchowdary01@gmail.com>
2018-03-10 16:53:35 +0530

    Pd.Dropna

--- DataCamp/Lectures/PythonCrashCourse/PandasMissingData.py
+++ DataCamp/Lectures/PythonCrashCourse/PandasMissingData.py
@@ -4,11 +4,11 @@ import numpy as np
 import pandas as pd
 
 population = [[1999,2344,1233,np.nan,np.nan,3455],
-              [np.nan,np.nan,4567,4343,5676,np.nan],
+              [2344,np.nan,4567,4343,5676,np.nan],
               [3456,7654,9876,8766,4545,2111],
-              [np.nan,np.nan,np.nan,4322,2322],
+              [6787,np.nan,np.nan,4322,2322],
               [3422,1212,2222,2344,np.nan,np.nan],
-              [np.nan,1234,np.nan,3456,np.nan,3434]]
+              [1002,1234,np.nan,3456,np.nan,3434]]
 
 years = [2000,2001,2002,2003,2004,2005]
 
@@ -16,4 +16,9 @@ countries = ['USA','China','India','Russia','Japan','Pakistan']
 
 data = pd.DataFrame(data= population, index=countries, columns =years)
 
-print(data)
+databck = data[:] #backup data
+
+print(data,'\n')
+#Thresh is minimum number of data the is not nan to be present in column/ row to be retained. axis = 0 is for rows , 1 is for columns
+
+#print(data.dropna(axis = 0, thresh=5))
